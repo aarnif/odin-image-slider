a6f2cfa1fe0ec1b154a63eb3c8d7cc47ed8d14aa
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-04-10 17:08:44 +0300

    Add styles to header and footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./dist/*.html", "./src/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        rotateAndScale: {
+          "0%": { transform: "rotate(0) scale(1)" },
+          "100%": { transform: "rotate(360deg) scale(1.5)" },
+        },
+      },
+      animation: {
+        "rotate-and-scale": "rotateAndScale 500ms ease-in-out forwards",
+      },
+    },
   },
   plugins: [],
 };
