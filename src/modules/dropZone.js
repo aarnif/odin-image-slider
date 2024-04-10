27e7e46c7399c145bbad3a4baa0a4a727e399fa6
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-04-10 18:57:35 +0300

    Add styles to drop-zone

--- src/modules/dropZone.js
+++ src/modules/dropZone.js
@@ -5,6 +5,15 @@ const dropZone = (images) => {
   const dropZoneContainer = document.createElement("div");
   dropZoneContainer.classList.add("drop-zone-container");
 
+  const dropZoneWaveOne = document.createElement("div");
+  dropZoneWaveOne.classList.add("drop-zone-wave");
+  const dropZoneWaveTwo = document.createElement("div");
+  dropZoneWaveTwo.classList.add("drop-zone-wave");
+  const dropZoneWaveThree = document.createElement("div");
+  dropZoneWaveThree.classList.add("drop-zone-wave");
+  const dropZoneWaveFour = document.createElement("div");
+  dropZoneWaveFour.classList.add("drop-zone-wave");
+
   const dropZone = document.createElement("div");
   dropZone.classList.add("drop-zone");
   dropZone.textContent = "Drop files here";
@@ -12,6 +21,21 @@ const dropZone = (images) => {
   dropZone.addEventListener("dragover", async (e) => {
     e.preventDefault();
     console.log("Files are over the drop zone");
+    dropZone.classList.add("active");
+    dropZoneWaveOne.classList.add("one");
+    dropZoneWaveTwo.classList.add("two");
+    dropZoneWaveThree.classList.add("three");
+    dropZoneWaveFour.classList.add("four");
+  });
+
+  dropZone.addEventListener("dragleave", (e) => {
+    e.preventDefault();
+    console.log("Files have left the drop zone");
+    dropZone.classList.remove("active");
+    dropZoneWaveOne.classList.remove("one");
+    dropZoneWaveTwo.classList.remove("two");
+    dropZoneWaveThree.classList.remove("three");
+    dropZoneWaveFour.classList.remove("four");
   });
 
   dropZone.addEventListener("drop", (e) => {
@@ -21,6 +45,11 @@ const dropZone = (images) => {
       displayService.showPreview(images);
     });
   });
+
+  dropZone.appendChild(dropZoneWaveOne);
+  dropZone.appendChild(dropZoneWaveTwo);
+  dropZone.appendChild(dropZoneWaveThree);
+  dropZone.appendChild(dropZoneWaveFour);
 
   dropZoneContainer.appendChild(dropZone);
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,23 @@ module.exports = {
           "0%": { transform: "rotate(0) scale(1)" },
           "100%": { transform: "rotate(360deg) scale(1.5)" },
         },
+        wave: {
+          "0%": {
+            transform: "scale(1)",
+            opacity: "0.3",
+          },
+          "100%": {
+            transform: "scale(1.2)",
+            opacity: "0",
+          },
+        },
       },
       animation: {
         "rotate-and-scale": "rotateAndScale 500ms ease-in-out forwards",
+        "wave-one": "wave 2s ease-in-out infinite",
+        "wave-two": "wave 2s 0.5s ease-in-out infinite",
+        "wave-three": "wave 2s 1s ease-in-out infinite",
+        "wave-four": "wave 2s 1.5s ease-in-out infinite",
       },
     },
   },
